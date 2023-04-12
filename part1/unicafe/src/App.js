402f5bad0e32e51de278eae832fb3277233fee7a
Carolina Bacelar <carolrbacelar@hotmail.com>
2023-04-12 17:17:53 +0100

    complete exercise 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -29,12 +29,14 @@ const Statistics = ({ good, neutral, bad }) => {
 
   return (
     <table>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={total} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive + "%"} />
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive + "%"} />
+      </tbody>
     </table>
   )
 }
