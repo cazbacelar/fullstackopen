9bf56ddc02debc8964241bc1c6a0fd2c34e74d23
Carolina Bacelar <carolrbacelar@hotmail.com>
2023-04-12 14:13:55 +0100

    complete unicafe exercise

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,36 +1,59 @@
-import { useState } from "react";
+import { useState } from "react"
 
-const Statistics = (props) => {
+const Button = ({ handleClick, text }) => {
   return (
-    <div>
-      <p>
-        {props.text} {props.value}
-      </p>
-    </div>
-  );
-};
+    <>
+      <button onClick={handleClick}>{text}</button>
+    </>
+  )
+}
+
+// for displaying a single statistic line
+const StatisticLine = (props) => {
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  const average = total / 3
+  const positive = (good / total) * 100
+
+  if (total === 0) {
+    return <div> No feedback given. </div>
+  }
+
+  return (
+    <table>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive + "%"} />
+    </table>
+  )
+}
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
       <h1>Give feedback</h1>
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <h2>Statistics</h2>
-      <Statistics text="good" value={good} />
-      <Statistics text="neutral" value={neutral} />
-      <Statistics text="bad" value={bad} />
-      <Statistics text="all" value={good + neutral + bad} />
-      <Statistics text="average" value={(good + neutral + bad) / 3} />
-      <Statistics text="positive" value={(good / (good + neutral + bad)) * 100} />
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
