6b61fac4ce8db4f893c4c8e4d54b8adc7e374072
Carolina Bacelar <carolrbacelar@hotmail.com>
2023-04-16 17:20:51 +0100

    complete exercises 2.4-2.5

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,59 @@
+const Total = ({ parts }) => {
+  //const total = parts.map((part) => part.exercises).reduce((a, b) => a + b, 0) -> using map and reduce
+
+  // using reduce with an array of objects to sum one of the properties of each -> returns an object with key:sum
+  // so I destructured the object returned
+  const { exercises } = parts.reduce((a, b) => ({
+    exercises: a.exercises + b.exercises,
+  }))
+
+  return (
+    <div>
+      <p>Total of {exercises} exercises</p>
+    </div>
+  )
+}
+
+const Part = ({ part, exercises }) => {
+  return (
+    <div>
+      <p>
+        {part} {exercises}
+      </p>
+    </div>
+  )
+}
+
+const Content = ({ parts }) => {
+  // parts is an array of objects
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  )
+}
+
+const Header = ({ name }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+    </div>
+  )
+}
+
+const Course = (props) => {
+  const { name, parts } = props.course
+  // props.course is the course object{}
+  // parts is an array of objects
+  return (
+    <div>
+      <Header name={name} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default Course
