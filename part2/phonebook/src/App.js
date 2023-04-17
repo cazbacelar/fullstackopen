3d2f4e045d660f1f24dc6ef03affa6f88afa1f46
Carolina Bacelar <carolrbacelar@hotmail.com>
2023-04-17 13:02:50 +0100

    compelte exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,23 @@ import { useState } from "react"
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ])
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
+  const [newSearch, setNewSearch] = useState("")
+  const [allPersons, setAllPersons] = useState(persons)
+
+  const handleSearchChange = (event) => {
+    // store the current valute in a variable so we can use it for the search, otherwise we only get the current value when this function is called again
+    const currentSearchValue = event.target.value
+    setNewSearch(currentSearchValue)
+    const regex = new RegExp( currentSearchValue, 'i' );
+    setPersons(allPersons.filter(person => person.name.match(regex) || person.number.match(regex)))
+  }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -29,6 +42,7 @@ const App = () => {
       const newPerson = {
         name: newName,
         number: newNumber,
+        id: persons.length + 1,
       }
       setPersons(persons.concat(newPerson))
       setNewName("")
@@ -40,7 +54,11 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        Search <input value={newSearch} onChange={handleSearchChange} />
+      </div>
+      <h2>Add a new contact</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -54,7 +72,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.name}>
+        <p key={person.id}>
           {person.name} {person.number}
         </p>
       ))}
