696811efb157e1becc6c41ecbe39b733943587ac
Carolina Bacelar <carolrbacelar@hotmail.com>
2023-05-03 00:57:50 +0100

    npm run build

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 import axios from "axios"
+
+// fetches the data from node/express-server that runs in localhost:3001
 const baseUrl = "http://localhost:3001/api/persons"
 
 const getAll = () => {
