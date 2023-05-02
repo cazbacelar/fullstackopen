b03fe5842242b135f5219606c35057342ea3b10f
Carolina Bacelar <carolrbacelar@hotmail.com>
2023-05-03 00:58:29 +0100

    declare baseUrl as a relative URL

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -2,11 +2,11 @@
 import axios from "axios"
 
 // fetches the data from node/express-server that runs in localhost:3001
-const baseUrl = "http://localhost:3001/api/persons"
+const baseUrl = "/api/persons"
 
 const getAll = () => {
   const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+  return request.then((response) => response.data)
 }
 
 // const getAll = () => {
