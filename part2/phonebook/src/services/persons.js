8a7817ca42dc58337aae6c407d3cf3ee7bb64bab
Carolina Bacelar <carolrbacelar@hotmail.com>
2023-05-02 22:18:22 +0100

    change URL structure to connect to the Node.js backend

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,30 +1,30 @@
 /* eslint-disable import/no-anonymous-default-export */
-import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
-
-// const getAll = () => {
-//   const request = axios.get(baseUrl)
-//   return request.then(response => response.data)
-// }
+import axios from "axios"
+const baseUrl = "http://localhost:3001/api/persons"
 
 const getAll = () => {
   const request = axios.get(baseUrl)
-  const nonExisting = {
-    id: 10000,
-    name: 'John Doe',
-    number: '123-4234324',
-  }
-  return request.then(response => response.data.concat(nonExisting))
+  return request.then(response => response.data)
 }
 
-const create = newObject => {
+// const getAll = () => {
+//   const request = axios.get(baseUrl)
+//   const nonExisting = {
+//     id: 10000,
+//     name: "John Doe",
+//     number: "123-4234324",
+//   }
+//   return request.then((response) => response.data.concat(nonExisting))
+// }
+
+const create = (newObject) => {
   const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
+  return request.then((response) => response.data)
 }
 
 const update = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
+  return request.then((response) => response.data)
 }
 
 const destroy = (id) => {
